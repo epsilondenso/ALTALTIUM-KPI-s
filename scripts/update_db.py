d08592b335680122f3acfeab68f312fca53a536a
epsilondenso <densel.pg@gmail.com>
2026-09-09 14:31:04 -0600

    Se trabaja en un script para actualizar la base de datos cada semana

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -1,8 +1,27 @@
 import time
+import yaml
+import pandas as pd
 from src.db.connect import get_connection
 from src.db.queries.semanas import insertar_siguiente_semana
+from src.db.insert_table import insert_table
+from src.clean_tables import load_table
+from src.utils import get_files
+from config.paths import SCHEMAS, DATA, CRM, CITAS, INTERESADOS, ESTAD_PORTALES
+
 
 if __name__ == "__main__":
+
+    ###################################
+    #--CARGAR TABLAS DESDE LOS CSV'S--#
+    ###################################
+    leads_crm_path = get_files(CRM)[1]
+    crm_df = load_table(path = CRM/leads_crm_path,
+           yaml_path = SCHEMAS/"leads_table.YAML")
+    crm_df
+
+    ################################
+    #--ACTUALIZAR TABLAS DE LA DB--#
+    ################################
     print(f"Estableciendo conexión con la db")
     #time.sleep(3)
     conn = get_connection()
@@ -10,7 +29,19 @@ if __name__ == "__main__":
     print("Actualizando tabla de semanas")
     #time.sleep(3)
     insertar_siguiente_semana(conn)
-    print("Tabla de semanas actualizada")
-    #time.sleep(3)
     conn.commit()
+    print("Tabla de semanas actualizada")
+    print("Actualizando tabla leads_crm")
+    try:
+        insert_table(
+            crm_df,
+            SCHEMAS/"leads_table.YAML",
+            conn
+            )
+    except:
+        print("Ocurrió algún error")
+        raise
+    #time.sleep(3)
+
+    
     print("Sesión cerrada") 
