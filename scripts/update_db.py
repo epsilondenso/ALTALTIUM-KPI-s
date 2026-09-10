7e1ae848d2661f5e74dd8f79ba460c4dea737c59
epsilondenso <densel.pg@gmail.com>
2026-09-10 17:44:20 -0600

    feat: update_db.py se ejecuta el flujo completo con los archivos históricos y de prueba

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -1,12 +1,10 @@
-import time
-import yaml
-import pandas as pd
 from src.db.connect import get_connection
 from src.db.queries.semanas import insertar_siguiente_semana
 from src.db.insert_table import insert_table
 from src.clean_tables import load_table
 from src.utils import get_files
-from config.paths import SCHEMAS, DATA, CRM, CITAS, INTERESADOS, ESTAD_PORTALES
+from src.preprocessing import concat_tables
+from config.paths import SCHEMAS, CRM, CITAS, INTERESADOS, ESTAD_PORTALES
 
 
 if __name__ == "__main__":
@@ -14,10 +12,34 @@ if __name__ == "__main__":
     ###################################
     #--CARGAR TABLAS DESDE LOS CSV'S--#
     ###################################
-    leads_crm_path = get_files(CRM)[1]
+        # -- leads_crm -- # 
+    leads_crm_path = get_files(CRM)[0]
     crm_df = load_table(path = CRM/leads_crm_path,
-           yaml_path = SCHEMAS/"leads_table.YAML")
-    crm_df
+                        yaml_path = SCHEMAS/"leads_table.YAML")
+        # -- citas -- # 
+    citas_path = get_files(CITAS)[0]
+    citas_df = load_table(path = CITAS / citas_path,
+                          yaml_path= SCHEMAS/ "citas_table.YAML")
+        # -- Estadísticas -- #
+    stats_tables = get_files(ESTAD_PORTALES)
+    concat = concat_tables(dir = ESTAD_PORTALES, #CONCATENAR LAS TABLAS Y GUARDAR como CSV
+                           output = ESTAD_PORTALES / "stats.csv")
+    
+    stats_df = load_table(path = ESTAD_PORTALES / "stats.csv",
+                          yaml_path= SCHEMAS / "stats.YAML"
+                          )
+        # -- Interesados -- #
+    inter_tables = get_files(ESTAD_PORTALES)
+    concat_inter = concat_tables(dir = INTERESADOS, #CONCATENAR LAS TABLAS Y GUARDAR como CSV
+                                 output = INTERESADOS / "interesados.csv",
+                                 add_portal_column= False)
+    
+    inter_df = load_table(path = INTERESADOS / "interesados.csv",
+                          yaml_path= SCHEMAS / "interesados_table.YAML"
+                          )
+    tables = [crm_df, citas_df, stats_df, inter_df]
+    yamls = ["leads_table.YAML", "citas_table.YAML", "stats.YAML", "interesados_table.YAML"]
+    table_names = ["leads_crm", "citas", "stats_portales", "interesados"]
 
     ################################
     #--ACTUALIZAR TABLAS DE LA DB--#
@@ -26,22 +48,24 @@ if __name__ == "__main__":
     #time.sleep(3)
     conn = get_connection()
     print("Conexión establecida")
-    print("Actualizando tabla de semanas")
+    print("Actualizando tabla semanas")
     #time.sleep(3)
     insertar_siguiente_semana(conn)
     conn.commit()
+    #conn.close()
     print("Tabla de semanas actualizada")
-    print("Actualizando tabla leads_crm")
-    try:
-        insert_table(
-            crm_df,
-            SCHEMAS/"leads_table.YAML",
-            conn
-            )
-    except:
-        print("Ocurrió algún error")
-        raise
+    for i in range(len(tables)):
+        print(f"Actualizando tabla {table_names[i]}")
+        try:
+            conn = get_connection()
+            insert_table(
+                tables[i],
+                SCHEMAS/yamls[i],
+                conn
+                )
+            print(f"Tabla {table_names[i]} actualizada")
+        except:
+            print("Ocurrió algún error")
+            raise
     #time.sleep(3)
-
-    
     print("Sesión cerrada") 
