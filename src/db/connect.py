b0631b3e99c46ca9fae0d55100a8da236dc8cba7
epsilondenso <densel.pg@gmail.com>
2026-09-18 15:00:59 -0600

    cleaned: codigo en desuso

--- src/db/connect.py
+++ src/db/connect.py
@@ -1,10 +1,15 @@
 import psycopg2
-
+import streamlit as st
 def get_connection(host: str = "localhost",
                   port: int = 5432, 
                   dbname: str = "kpis_altaltium",
                   user: str = "postgres",
                   password: str = "aVs#1105"):
+
+    if "connections" in st.secrets:
+        return psycopg2.connect(
+            st.secrets["connections"]["neon"]["url"]
+        )
     
     conn = psycopg2.connect(
         host="localhost",
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -46,64 +46,6 @@ def concat_tables(dir: str|Path,
         concat.to_csv(path_or_buf= output, index= False)
     return concat if return_concat else None 
 
-def get_df(object: str|Path|pd.DataFrame, start: int = 2, stop: int = 4) -> pd.DataFrame:
-    """
-    Obtiene un DataFrame desde un archivo, directorio o instancia existente.
-
-    Parameters
-    ----------
-    object : str | Path | pd.DataFrame
-        Fuente de datos a cargar o retornar.
-    start : int, optional
-        Índice inicial al concatenar archivos de un directorio.
-    stop : int, optional
-        Índice final al concatenar archivos de un directorio.
-
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame obtenido desde la fuente indicada.
-    """
-
-    if isinstance(object, Path):
-        return concat_tables(dir = object, start = start, stop = stop)
-
-    elif isinstance(object, str):
-        return pd.read_excel(object)
-
-    else:
-        return object
-
-def load_citas_df(citas_path: str):
-    """
-    Carga y limpia el archivo de citas.
-
-    Parameters
-    ----------
-    citas_path : str
-        Ruta del archivo CSV de citas.
-
-    Returns
-    -------
-    pd.DataFrame
-        DataFrame de citas con campos de texto normalizados.
-    """
-    citas = pd.read_csv(citas_path, 
-                    delimiter= ";", 
-                    header = 1, 
-                    usecols = ["N"] + columnas_archivo_citas,
-                    names = ["N"]+columnas_archivo_citas).dropna(subset = ["N"]+columnas_archivo_citas[:-1]).iloc[:, 1:]
-    strip_df(citas)
-
-    citas["ASISTENCIA"] = (
-        citas["ASISTENCIA"]
-        .str.lower()
-        .str.replace(" ", "", regex=False)
-        .str.replace(r"o$", "a", regex=True)
-    )
-    citas["VENTAS"] = citas["VENTAS"].fillna(0, inplace = False).apply(lambda x: 1.0 if not isinstance(x, int) else x)
-
-    return citas
 
 ###########################
 #-----REFACTORIZACIÓN-----#
