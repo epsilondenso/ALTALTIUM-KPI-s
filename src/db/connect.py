5cde93a26437b883476f063f7f267f071e6c0848
epsilondenso <densel.pg@gmail.com>
2026-09-18 17:27:16 -0600

    fixed: conexión con la db NEON

--- src/db/connect.py
+++ src/db/connect.py
@@ -1,22 +1,20 @@
 import psycopg2
 import streamlit as st
-def get_connection(host: str = "localhost",
-                  port: int = 5432, 
-                  dbname: str = "kpis_altaltium",
-                  user: str = "postgres",
-                  password: str = "aVs#1105"):
 
-    if "connections" in st.secrets:
-        return psycopg2.connect(
-            st.secrets["connections"]["neon"]["url"]
-        )
-    
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        dbname="kpis_altaltium",
-        user="postgres",
-        password="aVs#1105"
+def get_connection(
+    host: str = "localhost",
+    port: int = 5432,
+    dbname: str = "kpis_altaltium",
+    user: str = "postgres",
+    password: str = "TU_PASSWORD"
+):
+    if "DB_URL" in st.secrets:
+        return psycopg2.connect(st.secrets["DB_URL"])
+
+    return psycopg2.connect(
+        host=host,
+        port=port,
+        dbname=dbname,
+        user=user,
+        password=password
     )
-
-    return conn
